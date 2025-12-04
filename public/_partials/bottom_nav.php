43b8570803d2a853ad58fff13df837f816fba872
PEROVAS <lucas_pacheco5@estudante.sesisenai.org.br>
2025-12-04 08:30:55 -0300

    add_

--- public/_partials/bottom_nav.php
+++ public/_partials/bottom_nav.php
@@ -2,18 +2,28 @@
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 <div class="bottom-nav">
-  <a href="usuario_home.php" class="<?php echo $current_page == 'usuario_home.php' ? 'active' : ''; ?>">
+  <a href="dashboard.php" class="<?php echo $current_page == 'dashboard.php' ? 'active' : ''; ?>">
+    <i class="ri-dashboard-line"></i>
+    <span>Início</span>
+  </a>
+
+  <a href="avisos.php" class="<?php echo $current_page == 'avisos.php' ? 'active' : ''; ?>">
+    <i class="ri-notification-3-line"></i>
+    <span>Avisos</span>
+  </a>
+
+  <a href="rotas.php" class="<?php echo $current_page == 'rotas.php' ? 'active' : ''; ?>">
     <i class="ri-map-pin-line"></i>
     <span>Rotas</span>
   </a>
 
-  <a href="perfil_usuario.php" class="<?php echo $current_page == 'perfil_usuario.php' ? 'active' : ''; ?>">
-    <i class="ri-user-smile-line"></i>
-    <span>Perfil</span>
+  <a href="funcionarios.php" class="<?php echo $current_page == 'funcionarios.php' ? 'active' : ''; ?>">
+    <i class="ri-group-line"></i>
+    <span>Funcionários</span>
   </a>
 
-  <a href="logout.php">
-    <i class="ri-logout-box-r-line"></i>
-    <span>Sair</span>
+  <a href="meu_perfil.php" class="<?php echo $current_page == 'meu_perfil.php' ? 'active' : ''; ?>">
+    <i class="ri-user-settings-line"></i>
+    <span>Perfil</span>
   </a>
 </div>
--- public/_partials/bottom_nav_usuario.php
+++ public/_partials/bottom_nav_usuario.php
@@ -2,28 +2,18 @@
 $current_page = basename($_SERVER['PHP_SELF']);
 ?>
 <div class="bottom-nav">
-  <a href="dashboard.php" class="<?php echo $current_page == 'dashboard.php' ? 'active' : ''; ?>">
-    <i class="ri-dashboard-line"></i>
-    <span>Início</span>
-  </a>
-
-  <a href="avisos.php" class="<?php echo $current_page == 'avisos.php' ? 'active' : ''; ?>">
-    <i class="ri-notification-3-line"></i>
-    <span>Avisos</span>
-  </a>
-
-  <a href="rotas.php" class="<?php echo $current_page == 'rotas.php' ? 'active' : ''; ?>">
+  <a href="usuario_home.php" class="<?php echo $current_page == 'usuario_home.php' ? 'active' : ''; ?>">
     <i class="ri-map-pin-line"></i>
     <span>Rotas</span>
   </a>
 
-  <a href="funcionarios.php" class="<?php echo $current_page == 'funcionarios.php' ? 'active' : ''; ?>">
-    <i class="ri-group-line"></i>
-    <span>Funcionários</span>
+  <a href="perfil_usuario.php" class="<?php echo $current_page == 'perfil_usuario.php' ? 'active' : ''; ?>">
+    <i class="ri-user-smile-line"></i>
+    <span>Perfil</span>
   </a>
 
-  <a href="meu_perfil.php" class="<?php echo $current_page == 'meu_perfil.php' ? 'active' : ''; ?>">
-    <i class="ri-user-settings-line"></i>
-    <span>Perfil</span>
+  <a href="logout.php">
+    <i class="ri-logout-box-r-line"></i>
+    <span>Sair</span>
   </a>
 </div>
